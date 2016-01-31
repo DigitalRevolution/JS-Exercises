d4a740a90d06cd7f023de727b6e2f68d59236ad4
Digital Revolution <stephen.vandelinder@gmail.com>
2016-01-30 17:00:21 -0700

    KeyWord Builder latest update with array output as well as sheets coordinate output

--- kWbuilder/kWbuilder.js
+++ kWbuilder/kWbuilder.js
@@ -1,25 +1,29 @@
 ////////////////////////////KISS///////////////////////////////////////
 
+  var i,j,k,l; // incrementers 
+  var pre = ['How to ','Best way to ','Top ways to ']; // i
+  var verb = ['repair ','fix ','help ', 'cure '];  // j
+  var noun = ['knee pain ','knee inflamation ','knee soreness ','sore knees ','knee injury ']; // k
+  var prep = ['','']; // m
+  var resultArray = []; 
 
- var i,j,k,l; // incrementers 
-  var pre = ['How to remove ','How to delete ', 'Remove ', 'Delete ']; // i
-  var adj = ['','negative ','defamatory '];  // j
-  var noun = ['picture ','pictures ','article ','post ','posting ','listing ','review ','information ']; // k
-  var prep = ['on ','from ']; // m
-
-var kW = function(site, column){
-  var r =0; 
+var kW = function(suffix, column){
+  var r =2; // spreadsheet row to start on (initialize spreadsheet)
+  resultArray[column] = [];  // initialize array of resulting keywords 
     for(i=0; i<pre.length; i++){ // prefix loop
-        for(j=0; j<adj.length; j++){ // adj loop
+        for(j=0; j<verb.length; j++){ // verb loop
             for(k=0; k<noun.length; k++){ // noun loop
                 for(l=0; l<prep.length; l++){ // preposition loop
-                  console.log(column+':'+ r + pre[i]+adj[j]+noun[k]+prep[l]+site); 
-                  r++;
+                  console.log(column+':'+ r + '  ' + pre[i]+verb[j]+noun[k]+prep[l]+suffix); 
+                  // console.log here demonstrates an output with cell coords and the keyword
+                  // the application that uses this code outputs results into google Sheets
+                  // Alternatively, one can use JSON and multidiemtnsional arrays
+                  resultArray[column].push(pre[i]+verb[j]+noun[k]+prep[l]+suffix);
+                  r++; // move to the next row
                 } // close preposition loop
             } // close noun loop
-        } // close adj loop
+        } // close verb loop
     } // close preposition loop
+    return resultArray[column]; // return the full array
 };
-  kW('Blogspot',1); 
-
-
+  kW('fast',1); 
